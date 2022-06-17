80fe8444bef44a4413d906326e2a9b3295f6e6f6
unknown <nguyenhunganh072017@gmail.com>
2022-06-17 17:04:01 +0700

    Fix Responsive & add Slider

--- main.js
+++ main.js
@@ -10,3 +10,23 @@ toggle.addEventListener("click",function(e){
 //         headerMenu.classList.remove(isToggle);
 //     }
 // })
+
+// ---------------------------SCROLL ANIMATION------------------------
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+//   ---------------------------LIST SLIDER-----------
